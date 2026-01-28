d4ab2ee4bd827e833225c4c0d6a11fc38fceff29
TahmidRaven <raven.of.the.endless@gmail.com>
2026-01-28 18:25:40 +0600

    squoosh now runs on node v16

--- squoosh_batch.sh
+++ squoosh_batch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Switching to node16h
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 16 > /dev/null
 
 export NODE_OPTIONS="--no-experimental-fetch"
 
